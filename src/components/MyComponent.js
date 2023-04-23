43b7f485bf51d2bf0d56fb2123fc0475c91e0419
NamPV <nampv686@gmail.com>
2023-04-23 14:53:56 +0700

    #16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,18 @@
+//Class Component
+//Function Component
+
+import React from "react";
+
+class MyComponent extends React.Component {
+    JSX
+    render(){
+        return (
+            <div>
+                My first Component
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
