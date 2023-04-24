e0d1a52cf4358e86e1a0f2624a3f83121737ddb9
NamPV <nampv686@gmail.com>
2023-04-24 08:36:36 +0700

    #19.DOM Event #20.setState

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -9,12 +9,27 @@ class MyComponent extends React.Component {
         name: "Phi Nam",
         age: 24
     };
+    handleClick(event){
+        console.log(this.state.name);
+        this.setState(
+            {
+                name: "Hoàng" + Math.floor(Math.random() * 100),
+                age: Math.floor(Math.random() * 100)
+            }
+        );
+    }
+
+    handleOnMoverOver(event){
+        console.log(event);
+    }
 
     //JSX
     render(){
         return (
             <div>
                 My name is {this.state.name} and {this.state.age} years old
+                <button onMouseOver={this.handleOnMoverOver}>Hover</button>
+                <button onClick={(event) => {this.handleClick(event)}}>Click me</button>
             </div>
         );
     }
